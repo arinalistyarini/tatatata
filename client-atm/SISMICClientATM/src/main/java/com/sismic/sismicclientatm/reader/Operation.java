fb0442959ffa74b645c817c973b62d16a9a4d26b
arinalistyarini <arinalistyarini@yahoo.com>
2017-07-02 18:38:34 +0700

    atm ketinggalan

---
+++ client-atm/SISMICClientATM/src/main/java/com/sismic/sismicclientatm/reader/Operation.java
@@ -0,0 +1,181 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sismic.sismicclientatm.reader;
+
+import java.nio.ByteBuffer;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+import javax.xml.bind.DatatypeConverter;
+
+/**
+ *
+ * @author Arina Listyarini DA
+ */
+public final class Operation {
+     /** Regex pattern for hexadecimal strings */
+    private static final Pattern HEX_STRING_PATTERN = Pattern.compile("^([0-9A-Fa-f]{2})+$");
+    
+    /** Array of all hexadecimal chars */
+    private static final char[] HEX_CHARS = "0123456789ABCDEF".toCharArray();
+
+    public static boolean isHexString(String s) {
+        return (s != null) && HEX_STRING_PATTERN.matcher(s).matches();
+    }
+    
+    public static byte[] hexStringToBytes(String s) {
+        /*int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;*/
+        if (s.length() % 2 == 1){
+            s = "0" + s;
+        }
+        return DatatypeConverter.parseHexBinary(s);
+    }
+
+    public static String bytesToHexString(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int i = 0; i < bytes.length; i++) {
+            int v = bytes[i] & 0xFF;
+            hexChars[i * 2] = HEX_CHARS[v >>> 4];
+            hexChars[i * 2 + 1] = HEX_CHARS[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+    
+    public static String hexStringToASCII(String hex){
+
+	  StringBuilder sb = new StringBuilder();
+	  StringBuilder temp = new StringBuilder();
+
+	  //49204c6f7665204a617661 split into two characters 49, 20, 4c...
+	  for( int i=0; i<hex.length()-1; i+=2 ){
+
+	      //grab the hex in pairs
+	      String output = hex.substring(i, (i + 2));
+	      //convert hex to decimal
+	      int decimal = Integer.parseInt(output, 16);
+	      //convert the decimal to character
+	      sb.append((char)decimal);
+
+	      temp.append(decimal);
+	  }
+
+	return sb.toString();
+    }
+    
+    public static byte[] concat2Bytes(byte[] b1, byte[] b2){
+        byte[] combined = new byte[b1.length + b2.length];
+        System.arraycopy(b1, 0, combined, 0, b1.length);
+        System.arraycopy(b2, 0, combined, b1.length, b2.length); 
+        return combined;
+    }
+    
+    public static byte[] concat3Bytes(byte[] b1, byte[] b2, byte[] b3){
+        byte[] combined = new byte[b1.length + b2.length];
+        System.arraycopy(b1, 0, combined, 0, b1.length);
+        System.arraycopy(b2, 0, combined, b1.length, b2.length);
+        
+        byte[] combined2 = new byte[combined.length + b3.length];
+        System.arraycopy(combined, 0, combined2, 0, combined.length);
+        System.arraycopy(b3, 0, combined2, combined.length, b3.length);
+        return combined2;
+    }
+    
+    public static byte[] concat4Bytes(byte[] b1, byte[] b2, byte[] b3, byte[] b4){
+        byte[] combined = new byte[b1.length + b2.length];
+        System.arraycopy(b1, 0, combined, 0, b1.length);
+        System.arraycopy(b2, 0, combined, b1.length, b2.length);
+        
+        byte[] combined2 = new byte[combined.length + b3.length];
+        System.arraycopy(combined, 0, combined2, 0, combined.length);
+        System.arraycopy(b3, 0, combined2, combined.length, b3.length);
+        
+        byte[] result = new byte[combined2.length + b4.length];
+        System.arraycopy(combined2, 0, result, 0, combined2.length);
+        System.arraycopy(b4, 0, result, combined2.length, b4.length);
+        
+        return result;
+    }
+    
+    public static byte[] hexStringTo16Bytes(String data){
+        if (data.length() % 2 == 1){
+            data = "0" + data;
+        }
+        byte[] dataB = Operation.hexStringToBytes(data);
+        byte[] dataWrite = new byte[16];
+        System.arraycopy(dataB, 0, dataWrite, 16 - dataB.length, dataB.length);
+        return dataWrite;
+    }
+    
+    public static byte[] hexStringTo4Bytes(String data){
+        if (data.length() % 2 == 1){
+            data = "0" + data;
+        }
+        byte[] dataB = Operation.hexStringToBytes(data);
+        byte[] dataWrite = new byte[4];
+        System.arraycopy(dataB, 0, dataWrite, 4 - dataB.length, dataB.length);
+        return dataWrite;
+    }
+    
+    public static byte[] decimalIntegerToBytes(int dec){
+        String hex = Integer.toHexString(dec);
+        if (hex.length() % 2 == 1){
+            hex = "0" + hex;
+        }
+        byte[] result = hexStringToBytes(hex);
+        return result;
+    }
+    
+    public static byte[] decimalIntegerTo16Bytes(int dec){
+        String hex = Integer.toHexString(dec);
+        if (hex.length() == 1){
+            hex = "0" + hex;
+        }
+        byte[] result = hexStringToBytes(hex);
+        byte[] dataWrite = new byte[16];
+        System.arraycopy(result, 0, dataWrite, 16 - result.length, result.length);
+        return dataWrite;
+    }
+    
+    public static byte[] decimalIntegerTo4Bytes(int dec){
+        String hex = Integer.toHexString(dec);
+        if (hex.length() == 1){
+            hex = "0" + hex;
+        }
+        byte[] result = hexStringToBytes(hex);
+        byte[] dataWrite = new byte[4];
+        System.arraycopy(result, 0, dataWrite, 4 - result.length, result.length);
+        return dataWrite;
+    }
+    
+    public static int hexStringToDecimalInteger(String data){
+        int dec = (int) Long.parseLong(data, 16);
+        return dec;
+    }
+    
+    public static int bytesToDecimalInteger(byte[] b){
+        String data = bytesToHexString(b);
+        int dec = (int) Long.parseLong(data, 16);
+        return dec;
+    }
+    
+    public static String getBlockACRHexString(int sector, int block){
+        int sumBlock = (sector * 4) + block;
+        byte[] hexString = decimalIntegerToBytes(sumBlock);
+        String result = bytesToHexString(hexString);
+        return result;
+    }
+    
+    public static Date longToDate(long d){
+        Date date = new Date(TimeUnit.SECONDS.toMillis(d));
+        return date;
+    }
+}
---
+++ client-atm/SISMICClientATM/src/main/java/com/sismic/sismicclientatm/reader/Reader.java
@@ -0,0 +1,384 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sismic.sismicclientatm.reader;
+
+import java.io.IOException;
+import java.util.List;
+import javax.smartcardio.Card;
+import javax.smartcardio.CardChannel;
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+import javax.smartcardio.TerminalFactory;
+
+/**
+ *
+ * @author Arina Listyarini DA
+ */
+public final class Reader {
+    private static CardTerminal terminal = null;
+    private static CardChannel channel = null;
+    
+    public static void connectToReader() throws IOException {
+        // reader
+        try {
+            TerminalFactory factory = TerminalFactory.getDefault();
+            List<CardTerminal> terminalsList = factory.terminals().list();
+            terminal = terminalsList.get(0);
+
+            //System.out.println("[INFO] Terminal used: " + terminalsList.get(0));
+        } catch (CardException ex) {
+            System.out.println("[ERROR] Check if any terminal is connected!");
+            System.exit(-1);
+        }
+    }
+    
+    public static void readBlock(String block, String key, int aOrB){
+        //convert hexString to bytes
+        byte[] blockBy = Operation.hexStringToBytes(block);
+        byte[] keyBy = Operation.hexStringToBytes(key);
+        try {            
+            Card card = terminal.connect("*");
+            channel = card.getBasicChannel();
+
+            //Reset the card
+            card.getATR();
+                        
+            // load auth key
+            int loadKeyResult = apduLoadAuthKey(keyBy);
+            
+            //jika berhasil load key
+            if (loadKeyResult == 0x90) {                
+                // authenticate block
+                // key a or key b. aOrB = 0 key A, aOrB = 1 key B
+                int authBlockResult = 0;
+                if(aOrB == 0){
+                    authBlockResult = apduAuthBlockKeyA(blockBy);
+                }
+                else {
+                    authBlockResult = apduAuthBlockKeyB(blockBy);
+                }
+                                
+                // jika authBlock berhasil
+                if (authBlockResult == 0x90) {
+                    //baca isi blocknya
+                    byte[] read_block_apdu = new byte[] { 
+                        (byte) 0xFF,
+                        (byte) 0xB0,
+                        (byte) 0x00,
+                        // ....(byte) block
+                        // ....(byte) read_block_apdu_last
+                    };
+                    byte[] read_block_apdu_last = new byte[] {
+                        (byte) 0x10, // number byte to read, max 16bytes
+                    };
+                    read_block_apdu = Operation.concat3Bytes(read_block_apdu, blockBy, read_block_apdu_last);
+                    
+                    CommandAPDU cmd = new CommandAPDU(read_block_apdu);
+                    ResponseAPDU transmit = channel.transmit(cmd);
+                
+                    if (transmit.getSW1() == 0x90) {
+                        String result_hexstring = Operation.bytesToHexString(transmit.getData());
+                        String result_ascii = Operation.hexStringToASCII(result_hexstring);
+                        System.out.println("Isi block ke-" + block + ": " + result_hexstring + "(ASCII: " + result_ascii + ")" + " Decimal: " + Operation.hexStringToDecimalInteger(result_hexstring));
+                    }
+                    else{
+                        System.out.println("gagal baca isi block");
+                    }
+                }
+                else{
+                    System.out.println("[ERROR] Failed authenticating block");
+                }
+            }
+            else {
+                System.out.println("[ERROR] Failed loading auth key");
+            }
+         }
+         catch (CardException ex) {
+            System.out.println("[ERROR] No card found");
+            System.exit(-1);
+        }
+    }
+    
+    public static void readValueBlock(String block, String key, int aOrB){
+        //convert hexString to bytes
+        byte[] blockBy = Operation.hexStringToBytes(block);
+        byte[] keyBy = Operation.hexStringToBytes(key);
+        try {            
+            Card card = terminal.connect("*");
+            channel = card.getBasicChannel();
+
+            //Reset the card
+            card.getATR();
+                        
+            // load auth key
+            int loadKeyResult = apduLoadAuthKey(keyBy);
+            
+            //jika berhasil load key
+            if (loadKeyResult == 0x90) {                
+                // authenticate block
+                // key a or key b. aOrB = 0 key A, aOrB = 1 key B
+                int authBlockResult = 0;
+                if(aOrB == 0){
+                    authBlockResult = apduAuthBlockKeyA(blockBy);
+                }
+                else {
+                    authBlockResult = apduAuthBlockKeyB(blockBy);
+                }
+                
+                // jika authBlock berhasil
+                if (authBlockResult == 0x90) {
+                    byte[] value_block_read_apdu = new byte[] {
+                        (byte) 0xFF, (byte) 0xB1, (byte) 0x00,
+                        //...block number
+                        //(byte) 0x04
+                    };
+                    byte[] value_block_apdu_last = new byte[] {
+                        (byte) 0x04,
+                    };
+                    value_block_read_apdu = Operation.concat3Bytes(value_block_read_apdu, blockBy, value_block_apdu_last);
+                    CommandAPDU cmd = new CommandAPDU(value_block_read_apdu);
+                    ResponseAPDU transmit = channel.transmit(cmd);
+                    //jika berhasil baca
+                    if (transmit.getSW1() == 0x90) {                
+                        System.out.println("Isi value block sekarang adalah: " + Operation.bytesToDecimalInteger(transmit.getData()));
+                    }
+                    else {
+                        System.out.println("[ERROR] Gagal baca value block");
+                    }
+                }
+                else{
+                    System.out.println("[ERROR] Failed authenticating block");
+                }
+            }
+            else {
+                System.out.println("[ERROR] Failed loading auth key");
+            }
+         }
+         catch (CardException ex) {
+            System.out.println("[ERROR] No card found");
+            System.exit(-1);
+        }
+    }
+        
+    public static void writeBlock(String block, String key, int data, int aOrB){
+        //convert hexString to bytes
+        byte[] blockBy = Operation.hexStringToBytes(block);
+        byte[] keyBy = Operation.hexStringToBytes(key);
+        //byte[] dataBy = Operation.hexStringTo16Bytes(data); // kalo data nya string
+        byte[] dataBy = Operation.decimalIntegerTo16Bytes(data); // kalo data nya int
+        try {            
+            Card card = terminal.connect("*");
+            channel = card.getBasicChannel();
+
+            //Reset the card
+            card.getATR();
+            
+            // load auth key
+            int loadKeyResult = apduLoadAuthKey(keyBy);
+            
+            //jika berhasil load key
+            if (loadKeyResult == 0x90) {                
+                // authenticate block
+                // key a or key b. aOrB = 0 key A, aOrB = 1 key B
+                int authBlockResult;
+                if(aOrB == 0){
+                    authBlockResult = apduAuthBlockKeyA(blockBy);
+                }
+                else {
+                    authBlockResult = apduAuthBlockKeyB(blockBy);
+                }
+                
+                // jika authBlock berhasil
+                if (authBlockResult == 0x90) {
+                    //tulis block
+                    byte[] write_block_apdu = new byte[] { 
+                        (byte) 0xFF,(byte) 0xD6, (byte) 0x00,
+                    };
+                    byte[] write_block_apdu_between = new byte[] { 
+                        (byte) 0x10 // number of bytes to update (max 16 bytes)
+                    };
+                    write_block_apdu = Operation.concat4Bytes(write_block_apdu, blockBy, write_block_apdu_between, dataBy);
+                    
+                    CommandAPDU cmd = new CommandAPDU(write_block_apdu);
+                    ResponseAPDU transmit = channel.transmit(cmd);
+                
+                    if (transmit.getSW1() == 0x90) {
+                        System.out.println("[INFO] Block ke-" + block + " berhasil di-update)");
+                    }
+                    else{
+                        System.out.println("gagal tulis block");
+                    }
+                }
+                else{
+                    System.out.println("[ERROR] Failed authenticating block");
+                }
+            }
+            else {
+                System.out.println("[ERROR] Failed loading auth key");
+            }
+         }
+         catch (CardException ex) {
+            System.out.println("[ERROR] No card found");
+            System.exit(-1);
+        }
+    }
+    
+    public static void writeValueBlock(String block, String key, int data, int aOrB, int incdec){
+        //convert hexString to bytes
+        byte[] blockBy = Operation.hexStringToBytes(block);
+        byte[] keyBy = Operation.hexStringToBytes(key);
+        //byte[] dataBy = Operation.hexStringTo4Bytes(data); // kalo data nya string
+        byte[] dataBy = Operation.decimalIntegerTo4Bytes(data); // kalo data nya int
+        byte[] inc = new byte[] {
+            (byte) 0x01,
+        };
+        byte[] dec = new byte[] {
+            (byte) 0x02,
+        };
+        try {            
+            Card card = terminal.connect("*");
+            channel = card.getBasicChannel();
+
+            //Reset the card
+            card.getATR();
+                        
+            // load auth key
+            int loadKeyResult = apduLoadAuthKey(keyBy);
+            
+            //jika berhasil load key
+            if (loadKeyResult == 0x90) {                
+                // authenticate block
+                // key a or key b. aOrB = 0 key A, aOrB = 1 key B
+                int authBlockResult = 0;
+                if(aOrB == 0){
+                    authBlockResult = apduAuthBlockKeyA(blockBy);
+                }
+                else {
+                    authBlockResult = apduAuthBlockKeyB(blockBy);
+                }
+                
+                // jika authBlock berhasil
+                if (authBlockResult == 0x90) {
+                    byte[] value_block_operation_apdu = new byte[] {
+                        (byte) 0xFF, (byte) 0xD7, (byte) 0x00,
+                        //(byte) 0x10, // block number
+                        // (byte) 0x05,
+                        // (byte) 0x01, // increment: 01h, decrement: 02h
+                        // (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0xFF // data to be incremented. signed long integer 4 bytes
+                    };
+                    byte[] value_block_operation_apdu_middle = new byte[] {
+                        (byte) 0x05,
+                    };
+                    byte[] value_block_operation_apdu_last;
+                    if(incdec == 0){ //increment
+                        value_block_operation_apdu_last = Operation.concat2Bytes(inc, dataBy);
+                    }
+                    else {
+                        value_block_operation_apdu_last = Operation.concat2Bytes(dec, dataBy);
+                    }
+                    value_block_operation_apdu = Operation.concat4Bytes(value_block_operation_apdu, blockBy, value_block_operation_apdu_middle, value_block_operation_apdu_last);
+                    CommandAPDU cmd = new CommandAPDU(value_block_operation_apdu);
+                    ResponseAPDU transmit = channel.transmit(cmd);
+                    //jika berhasil increment
+                    if (transmit.getSW1() == 0x90) {                
+                        System.out.print("Value block berhasil ditulis. ");
+                    }
+                    else {
+                        System.out.println("[ERROR] Failed writing value block");
+                    }
+                }
+                else{
+                    System.out.println("[ERROR] Failed authenticating block");
+                }
+            }
+            else {
+                System.out.println("[ERROR] Failed loading auth key");
+            }
+         }
+         catch (CardException ex) {
+            System.out.println("[ERROR] No card found");
+            System.exit(-1);
+        }
+    }
+    
+    public static int apduAuthBlockKeyA(byte[] blockB) {
+        try{
+            byte[] authenticate_block_apdu = {
+                (byte) 0xFF, (byte) 0x86, (byte) 0x00, (byte) 0x00, (byte) 0x05,
+                (byte) 0x01, (byte) 0x00
+                // .....(byte) block
+                // .....(byte) authenticate_block_apdu_last
+            };
+            byte[] authenticate_block_apdu_last = {
+                (byte) 0x60, // key tipe. type a: 60, type b 61
+                (byte) 0x01 // key location
+            };
+            authenticate_block_apdu = Operation.concat3Bytes(authenticate_block_apdu, blockB, authenticate_block_apdu_last);
+
+            CommandAPDU cmd = new CommandAPDU(authenticate_block_apdu);
+            ResponseAPDU transmit = channel.transmit(cmd);
+
+            return transmit.getSW1();
+        }
+        catch (CardException ex) {
+            System.out.println("[ERROR] Something wrong with the card");
+            System.exit(-1);
+        }
+        return -1;
+    }
+    
+    public static int apduAuthBlockKeyB(byte[] blockB) {
+        try{
+            byte[] authenticate_block_apdu = {
+                (byte) 0xFF, (byte) 0x86, (byte) 0x00, (byte) 0x00, (byte) 0x05,
+                (byte) 0x01, (byte) 0x00
+                // .....(byte) block
+                // .....(byte) authenticate_block_apdu_last
+            };
+            byte[] authenticate_block_apdu_last = {
+                (byte) 0x61, // key tipe. type a: 60, type b 61
+                (byte) 0x01 // key location
+            };
+            authenticate_block_apdu = Operation.concat3Bytes(authenticate_block_apdu, blockB, authenticate_block_apdu_last);
+
+            CommandAPDU cmd = new CommandAPDU(authenticate_block_apdu);
+            ResponseAPDU transmit = channel.transmit(cmd);
+
+            return transmit.getSW1();
+        }
+        catch (CardException ex) {
+            System.out.println("[ERROR] Something wrong with the card");
+            System.exit(-1);
+        }
+        return -1;
+    }
+    
+    public static int apduLoadAuthKey(byte[] keyB){
+        try {
+            byte[] load_auth_key_apdu = {
+                (byte) 0xFF, (byte) 0x82, 
+                (byte) 0x00, 
+                (byte) 0x01, //key location
+                (byte) 0x06
+                // .....(byte) key
+            };
+            load_auth_key_apdu = Operation.concat2Bytes(load_auth_key_apdu, keyB);
+
+            CommandAPDU cmd = new CommandAPDU(load_auth_key_apdu);
+            ResponseAPDU transmit = channel.transmit(cmd);
+
+            return transmit.getSW1();
+        }
+        catch (CardException ex) {
+            System.out.println("[ERROR] Something wrong with the card");
+            System.exit(-1);
+        }
+        return -1;
+    }
+}
